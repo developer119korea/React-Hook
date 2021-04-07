7149d9934eae3fb65225f5e6ac8c0a9816604965
Youngwan Cho <developer119korea@gmail.com>
2021-04-07 23:02:17 +0900

    UseEffect를 활용해보기위해 웹페이지의 타이틀을 변경하는 기능을 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import { App as UseInput } from "./useInput";
 import { App as UseTabs } from "./useTabs";
+import { App as UseTitle } from "./useTitle";
 
 export default function App() {
   return (
     <div className="App">
       <UseInput/>
       <UseTabs/>
+      <UseTitle/>
     </div>
   );
 }
